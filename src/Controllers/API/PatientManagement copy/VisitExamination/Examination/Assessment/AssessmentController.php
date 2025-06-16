a68d26c3aa0e3b78516aaf18da3162d6b24eed9e
Hamzah <hamzahnafalah@gmail.com>
2025-06-16 15:57:31 +0700

    update modules

--- src/Controllers/API/PatientManagement copy/VisitExamination/Examination/Assessment/AssessmentController.php
+++ src/Controllers/API/PatientManagement copy/VisitExamination/Examination/Assessment/AssessmentController.php
@@ -19,10 +19,28 @@ class AssessmentController extends EnvironmentController
 
     public function show(ShowRequest $request){
         $data = $this->__assessment_schema->showAssessment();
-        if(isset($data)) return $data;
-
-        $flag = Str::studly(request()->flag);
-        $model = $this->{$flag.'Model'}();
-        return (\method_exists($model,'getExams')) ? $model->getExams() : null;
+        if(!isset($data)) {
+            $flag  = Str::studly(request()->flag);
+            $model = $this->{$flag.'Model'}();
+            $data  = (\method_exists($model,'getExams')) ? $model->getExams() : null;
+        }
+        $response = [
+            'visit_examiantion_id' => request()->visit_examination_id,
+        ];
+        $exam_type = request()->exam_type;
+        switch ($exam_type) {
+            case 'medical-support':
+            case 'prescription':
+            case 'examination':
+                $response[$exam_type] = [
+                    $flag => [
+                        'data' => $data
+                    ]
+                ];
+            break;
+            case 'treatments':
+                $response[$exam_type] = $data;
+            break;
+        }
     }
 }
---
+++ src/Transformers/Examination/ViewExamination.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Projects\Klinik\Transformers\Examination;
+
+use Hanafalah\LaravelSupport\Resources\ApiResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class ViewExamination extends ApiResource{
+    protected $__local_model;
+
+    public function toArray(Request $request): array
+    {
+        $model = $this->__local_model = $this->getModel();
+        $morph = Str::snake($model->getMorphClass());
+        return [
+            'visit_examination_id' => null,
+            'examination' => [
+                $morph => [
+                    'data' => $this->getAssessmentData()
+                ],
+            ]
+        ];
+    }
+
+    private function getAssessmentData(){
+        $response_as = $this->__local_model->response_model;
+        
+    }
+}
