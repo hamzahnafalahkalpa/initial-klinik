5c5c93496f5be5f32666ee029ade757d8e7b6fed
Hamzah <hamzahnafalah@gmail.com>
2025-06-18 01:16:53 +0700

    update modules

--- src/Controllers/API/Setting/EducationController.php
+++ src/Controllers/API/Setting/EducationController.php
@@ -5,7 +5,7 @@ namespace Projects\Klinik\Controllers\API\Setting;
 use Hanafalah\ModulePeople\Contracts\Schemas\Education;
 use Projects\Klinik\Controllers\API\ApiController;
 use Projects\Klinik\Requests\API\Setting\Education\{
-    ViewRequest, ShowRequest, StoreRequest, DeleteRequest
+    ViewRequest, StoreRequest, DeleteRequest
 };
 
 class EducationController extends ApiController{
@@ -15,10 +15,6 @@ class EducationController extends ApiController{
 
     public function index(ViewRequest $request){
         return $this->__schema->viewEducationList();
-    }
-
-    public function show(ShowRequest $request) {
-        return $this->__schema->showEducation();
     }
 
     public function store(StoreRequest $request){
