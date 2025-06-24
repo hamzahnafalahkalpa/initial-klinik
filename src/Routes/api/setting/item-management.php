18c104f09bfcb023b60089f402036ccb99eb8baf
Hamzah <hamzahnafalah@gmail.com>
2025-06-24 14:25:15 +0700

    modules update

--- src/Routes/api/setting/item-management.php
+++ src/Routes/api/setting/item-management.php
@@ -2,19 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 use Projects\Klinik\Controllers\API\Setting\{
-    SupplierController
+    BrandController,
+    DosageFormController,
+    PackageFormController,
+    TherapeuticClassController,
+    UsageLocationController,
+    UsageRouteController,
+    NetUnitController,
+    SellingFormController,
+    UnitOfMeasureController
 };
 
 Route::group([
     'prefix' => '/item-management',
     'as' => 'item-management.'
 ],function(){
-    Route::apiResource('/brand',SupplierController::class)->parameters(['brand' => 'id']);
-    Route::apiResource('/unit-of-measure',SupplierController::class)->parameters(['unit-of-measure' => 'id']);
-    Route::apiResource('/dosage-form',SupplierController::class)->parameters(['dosage-form' => 'id']);
-    Route::apiResource('/therapeutic-class',SupplierController::class)->parameters(['therapeutic-class' => 'id']);
-    Route::apiResource('/usage-route',SupplierController::class)->parameters(['usage-route' => 'id']);
-    Route::apiResource('/selling-form',SupplierController::class)->parameters(['selling-form' => 'id']);
-    Route::apiResource('/package-form',SupplierController::class)->parameters(['package-form' => 'id']);
+    Route::apiResource('/brand',BrandController::class)->parameters(['brand' => 'id']);
+    Route::apiResource('/net-unit',NetUnitController::class)->parameters(['net-unit' => 'id']);
+    Route::apiResource('/selling-form',SellingFormController::class)->parameters(['selling-form' => 'id']);
+    Route::apiResource('/unit-of-measure',UnitOfMeasureController::class)->parameters(['unit-of-measure' => 'id']);
+    Route::group([
+        'prefix' => '/medicine',
+        'as' => 'medicine.'
+    ],function(){
+        Route::apiResource('/dosage-form',DosageFormController::class)->parameters(['dosage-form' => 'id']);
+        Route::apiResource('/package-form',PackageFormController::class)->parameters(['package-form' => 'id']);
+        Route::apiResource('/therapeutic-class',TherapeuticClassController::class)->parameters(['therapeutic-class' => 'id']);
+        Route::apiResource('/usage-location',UsageLocationController::class)->parameters(['usage-location' => 'id']);
+        Route::apiResource('/usage-route',UsageRouteController::class)->parameters(['usage-route' => 'id']);
+    });
 });
 
